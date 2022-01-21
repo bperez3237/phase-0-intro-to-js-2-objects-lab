ef6cbaa88052dc9d0fa2f8c1d6c6e1ff93fa73b3
Brian <b.perez3237@gmail.com>
2022-01-20 19:39:46 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+
+const employee = {name:"Brian",streetAddress:"Duffield"};
+console.log(employee);
+
+function updateEmployeeWithKeyAndValue(employee,k,v) {
+    let newEmployee = {...employee};
+    newEmployee[k] = v;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,k,v) {
+    employee[k] = v;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,k) {
+    let newO = {...employee};
+    delete newO[k];
+    return newO;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,k) {
+    delete employee[k];
+    return employee;
+}
